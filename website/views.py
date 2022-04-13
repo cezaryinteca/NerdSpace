ea6a36e5bd0dc7428c4da0782b18ae84724242c6
Kuba <szurkowski.jakub@gmail.com>
2022-04-13 14:13:03 +0200

    Dodanie komentarzy

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
@@ -21,12 +21,12 @@ def create_post():
         text = request.form.get('text')
 
         if not text:
-            flash('Post nie może być pusty', category='error')
+            flash('Wpis nie może być pusty', category='error')
         else:
             post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-            flash('Post created!', category='success')
+            flash('Wpis utworzony!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template('create_post.html', user=current_user)
@@ -38,13 +38,13 @@ def delete_post(id):
     post = Post.query.filter_by(id=id).first()
 
     if not post:
-        flash("Post nie istnieje.", category='error')
+        flash("Wpis nie istnieje.", category='error')
     elif current_user.id != post.id:
-        flash('Nie masz uprawnień, aby usunąć ten post.', category='error')
+        flash('Nie masz uprawnień, aby usunąć ten wpis.', category='error')
     else:
         db.session.delete(post)
         db.session.commit()
-        flash('Post usunięty.', category='success')
+        flash('Wpis usunięty.', category='success')
 
     return redirect(url_for('views.home'))
 
@@ -58,5 +58,40 @@ def posts(username):
         flash('Nie istnieje użytkownik o tej nazwie.', category='error')
         return redirect(url_for('views.home'))
 
-    posts = Post.query.filter_by(author=user.id).all()
+    posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Komentarz nie może być pusty.', category = 'error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Wpis nie istnieje.', category='error')
+
+
+    return redirect(url_for('views.home'))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Komentarz nie istnieje', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('Brak uprawnień, aby wykonać tę operację.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
